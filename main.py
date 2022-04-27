2d944727ea71fb40955f97b0c428ea6f863d154e
nwelter1 <nwelter1@gmail.com>
2022-04-26 22:29:10 -0500

    jinja login

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from email import message
 from os import stat
-from fastapi import FastAPI, Request, Depends, HTTPException, Header
+from fastapi import FastAPI, Request, Depends, HTTPException, Header, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
@@ -82,10 +83,34 @@ def delete_job(user_id: str, job_id: str, db: Session = Depends(get_db), x_acces
     if verify_token(db=db, token=x_access_token, user_id=user_id):
         return crud.delete_job(db, user_id, job_id)
 
+# HTML/Frontend routing
+
+# Login Page
+@app.get('/loginpage', response_class=HTMLResponse)
+async def login_page(request: Request):
+    print(request.method)
+    return templates.TemplateResponse('pages/login.html', {'request': request})
 
 
-
-
+# Login Attempt
+@app.post('/login/')
+async def login_post(request: Request, email: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=email)
+    if not db_user:
+        raise HTTPException(status_code=404, detail='user not found')
+    if not db_user.verify_password(password):
+        raise HTTPException(status_code=401, detail='Ivalid pw for user')
+    
+    wishlist = [job for job in db_user.jobs if job.status == 'wishlist']
+    applied = [job for job in db_user.jobs if job.status == 'applied']
+    interviewing = [job for job in db_user.jobs if job.status == 'interviewing']
+    accepted = [job for job in db_user.jobs if job.status == 'accepted']
+    return templates.TemplateResponse('pages/profile.html', {'request': request, 
+    'user':db_user, 
+    'wishlist': wishlist,
+    'applied': applied,
+    'interviewing': interviewing,
+    'accepted': accepted})
 
 
 
